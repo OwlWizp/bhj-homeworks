206687eda75ebf0062dc890e6f683116f58487e3
Wizp <shadrawizp@gmail.com>
2024-07-16 17:50:52 +0300

    first commit ads

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,13 +1,19 @@
 const rotators = Array.from(document.querySelectorAll('.rotator'))
 
 
-function rotation(element) {
-    function replace() {
+function replace(element) {
         const activeCase = element.querySelector('.rotator__case_active');
-        activeCase.nextElementSibling.classList.add('rotator__case_active');
+        if (activeCase === element.lastElementChild) {
+            element.firstElementChild.classList.add('rotator__case_active');
+        } else {
+            activeCase.nextElementSibling.classList.add('rotator__case_active');
+        }
         activeCase.classList.remove('rotator__case_active');
     }
-    setInterval(replace(), 1000);
+
+function rotation(element) {    
+    let nIntervId = setInterval(() => replace(element), 1000)
 }
+
 
 rotators.forEach((element) => rotation(element));
